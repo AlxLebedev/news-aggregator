a122b7f8dd6ea3dfd0a9f654b1ab52381ba1429c
Aleksey Lebedev <aleksey.lebedev@realize.dev>
2020-07-08 13:21:46 +0300

    file-loader && image-webpack-loader added with optimization images

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -12,5 +12,16 @@ module.exports = merge(common, {
     new webpack.SourceMapDevToolPlugin({
       filename: '[file].map'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        }
+      }
+    ]
+  }
 });
--- webpack.prod.conf.js
+++ webpack.prod.conf.js
@@ -1,16 +1,40 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.conf');
 
-// const buildWebpackConfig = merge(common, {
-//   mode: 'production',
-//   plugins: []
-// })
-
-// module.exports = new Promise((resolve, reject) => {
-//   resolve(buildWebpackConfig)
-// })
-
 module.exports = merge(common, {
   mode: 'production',
-  plugins: []
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 75
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              svgo: {
+                removeViewBox: false,
+              }
+            }
+          },
+        ],
+      }
+    ]
+  }
 });
