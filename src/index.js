24674c537720a4a4053d25cd39ccaf1317429227
Aleksey Lebedev <aleksey.lebedev@realize.dev>
2020-07-08 18:00:25 +0300

    css removed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import './scss/style.scss';
-import './css/css-style.css';
 import './js/app';
--- webpack.common.conf.js
+++ webpack.common.conf.js
@@ -36,20 +36,6 @@ module.exports = {
         ],
       },
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true }
-          }, {
-            loader: 'postcss-loader',
-            options: { sourceMap: true, config: { path: './postcss.config.js' } }
-          }
-        ]
-      },
-      {
         test: /\.scss$/,
         use: [
           'style-loader',
