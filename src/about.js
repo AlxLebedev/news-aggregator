fd4138c78a5ea932753bb88a32352733176b53f3
Aleksey Lebedev <aleksey.lebedev@realize.dev>
2020-07-14 17:59:56 +0300

    fast-async added

--- src/about.js
+++ src/about.js
@@ -2,3 +2,14 @@ import './scss/styles-about.scss';
 import './js/about-app';
 import './js/sayAbout';
 
+let url = 'https://api.github.com/repos/AlxLebedev/news-aggregator/commits?per_page=5';
+
+// async function getCommits(url) {
+//   let response = await fetch(url);
+//   let commits = await response.json();
+//   return commits;
+// }
+
+// const commits = getCommits(url);
+// console.log(commits);
+
