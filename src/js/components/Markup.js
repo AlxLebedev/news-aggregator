e5eb891e54b56ee3ef77e98211b7a8148ba0fe01
Alexey Lebedev <vorresti@ya.ru>
2020-07-20 10:16:53 +0300

    image-not-found file replaced

--- src/js/components/Markup.js
+++ src/js/components/Markup.js
@@ -103,7 +103,7 @@ export default class Markup {
     <article class="article results__article">
       <a class="article__link" href="${articleData.url == null ? `` : articleData.url}" target="_blank">
         <div class="article__image-box">
-          <img class="article__image" src="${articleData.image == null ? `img/no.jpg` : articleData.image}" alt="картинка на сервере не найдена">
+          <img class="article__image" src="${articleData.image == null ? `img/image-not-found.jpg` : articleData.image}" alt="картинка на сервере не найдена">
         </div>
         <div class="article__wrapper">
           <div class="article__date">${articleData.date == null ? `дата не указана...` : articleData.date}</div>
