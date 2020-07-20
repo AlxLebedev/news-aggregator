4129bda14e78314c807487104b2b4d14c5995493
Alexey Lebedev <vorresti@ya.ru>
2020-07-20 13:11:01 +0300

    Analytics heading done

--- src/js/Modules/Analytics.js
+++ src/js/Modules/Analytics.js
@@ -5,9 +5,27 @@ export default class Analytics {
     this.drawUI = new DrawUI();
     this.data = data;
     this.query = userQuery;
+    this.referencesInHeadlines = null;
   }
 
   init() {
-    this.drawUI.showStatHeading(this.query, +this.data.totalResults, +'322322')
+    this.referencesInHeadlines = this.getReferencesInHeadlines();
+    this.drawUI.showStatHeading(this.query, +this.data.totalResults, this.referencesInHeadlines);
+  }
+
+  getReferencesInHeadlines() {
+    const headlines = [];
+    const regExp = new RegExp(this.query.toLowerCase(), 'g');
+    console.log(regExp);
+    for (let artickle of this.data.articles) {
+      headlines.push(artickle.title);
+    }
+    console.log(headlines);
+    const result = String(headlines).toLowerCase().match(regExp);
+    if (result) {
+      return result.length;
+    } else {
+      return 0;
+    }
   }
 }
