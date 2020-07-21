480f8643d43d58f5e44486a1842bafb21549aef3
Alexey Lebedev <vorresti@ya.ru>
2020-07-21 17:37:05 +0300

    GetNews cleaning

--- src/js/Modules/GetNews.js
+++ src/js/Modules/GetNews.js
@@ -8,30 +8,26 @@ export default class GetNews {
     this.dates = new Dates();
     this.drawUI = drawUI;
     this.externalApi = new ExternalApi(this.drawUI);
-    this.userQueryValid = null;
     this.userQuery = null;
     this.fromDate = null;
     this.toDate = null;
-    this.url = null;
-    this.newsApi = null;
-    this.news = null;
   }
 
   async get(userQuery) {
     this.userQuery = userQuery;
-    this.userQueryValid = this.validator.check(this.userQuery);
+    const userQueryValid = this.validator.check(this.userQuery);
 
-    if (!this.userQueryValid) {
+    if (!userQueryValid) {
       this.drawUI.showHint();
       return;
     }
     sessionStorage.setItem('userQuery', this.userQuery);
     [ this.fromDate, this.toDate ] = this.dates.getDatesForRequest();
-    this.url = `http://newsapi.org/v2/everything?q=${this.userQuery}&from=${this.fromDate}&to=${this.toDate}&language=ru&pageSize=100`;
-    this.news = await this.externalApi.fetchNews(this.url);
-    if (this.news.totalResults !== 0) {
-      this.drawUI.renderResultsContent(this.news);
-      sessionStorage.setItem('newsData', JSON.stringify(this.news));
+    const url = `http://newsapi.org/v2/everything?q=${this.userQuery}&from=${this.fromDate}&to=${this.toDate}&language=ru&pageSize=100`;
+    const news = await this.externalApi.fetchNews(url);
+    if (news.totalResults !== 0) {
+      this.drawUI.renderResultsContent(news);
+      sessionStorage.setItem('newsData', JSON.stringify(news));
     }
   }
 }
