faeb6c4192c4d2f4112f1d1173f7cbb71e907a80
Alexey Lebedev <vorresti@ya.ru>
2020-07-23 14:44:02 +0300

    Webpack output for JS changed

--- webpack.common.conf.js
+++ webpack.common.conf.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
+    filename: 'js/[name].[chunkhash].js',
   },
   plugins: [
     new WbebpackMd5Hash(),
