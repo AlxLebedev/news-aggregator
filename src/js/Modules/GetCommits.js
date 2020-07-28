8c1ce20fb8dfbd329681c379a8e8cc011c0d0da7
Alexey Lebedev <vorresti@ya.ru>
2020-07-28 17:45:57 +0300

    url to commits fixed

--- src/js/Modules/GetCommits.js
+++ src/js/Modules/GetCommits.js
@@ -5,7 +5,7 @@ export default class GetCommits {
   constructor() {
     this.drawUI = new DrawUI();
     this.externalApi = new ExternalApi(this.drawUI);
-    this.url = 'https://api.github.com/repos/AlxLebedev/news-aggregator/commits?sha=level-1&per_page=5';
+    this.url = 'https://api.github.com/repos/AlxLebedev/news-aggregator/commits?per_page=5';
     this.commits = null;
   }
 
