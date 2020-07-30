c1bd0e8b1990533c246dcf924596b5ac4b0f787e
Alexey Lebedev <vorresti@ya.ru>
2020-07-30 18:01:16 +0300

    restoring order in the file structure

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,8 @@ import '../img/svg/common/github.svg';
 
 import '../scss/styles-index.scss';
 
-import notFoundPic from '../img/static/not-found_v1.png';
-import serverError from '../img/static/server-error.png';
-
+import notFoundErrorPic from '../img/static/not-found-error.png';
+import serverErrorPic from '../img/static/server-error.png';
 
 import validateRequest from './utils/validate-request';
 import getDatesForNewsApi from './utils/get-dates-for-news-api';
@@ -15,18 +14,18 @@ import Preloader from './components/Preloader';
 import Error from './components/Error';
 import ResultsContainer from './components/ResultsContainer';
 import FinderInput from './components/FinderInput';
-import FinderSearch from './components/FinderSearch';
 import DataStorage from './Modules/DataStorage';
 import Articles from './components/Articles';
 import formatDate from './utils/format-date';
 import ShowMoreButton from './components/ShowMoreButton';
+import FinderSearch from './components/FinderSearch';
 
 const newsApi = new NewsApi(getDatesForNewsApi);
 const preloader = new Preloader();
-const error = new Error(notFoundPic, serverError);
+const error = new Error(notFoundErrorPic, serverErrorPic);
 const resultsContainer = new ResultsContainer();
-const dataStorage = new DataStorage();
 const finderInput = new FinderInput();
+const dataStorage = new DataStorage();
 const articles = new Articles(formatDate);
 const showMoreButton = new ShowMoreButton(articles);
 
