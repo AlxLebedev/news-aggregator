9c015f6f8a0452754b03acb908a841f2197108e7
Alexey Lebedev <vorresti@ya.ru>
2020-07-31 17:02:10 +0300

    Graph done

--- src/js/components/Graph.js
+++ src/js/components/Graph.js
@@ -38,8 +38,31 @@ export default class Graph {
   }
 
   renderReferencesByDays() {
+    const referencesValuesElements = Array.from(document.querySelectorAll('.graph__value'));
+    const topBarValues = document.querySelectorAll('.graph__bar-value-top');
+    const bottomBarValues = document.querySelectorAll('.graph__bar-value-bottom');
+
     const sortedArticles = this.sortArticlesByDays(this.news.articles, this.datesRange);
     const referencesByDays = this.getReferencesByDays(sortedArticles, this.request);
+    const totalReferences = referencesByDays.reduce( (sum, current) => sum + current );
+    const quarterOfTotalReferences = totalReferences / 4;
+
+    let counter = null;
+    for (let value of topBarValues) {
+      value.innerText = totalReferences <= 10 ? (counter + quarterOfTotalReferences).toFixed(1) : Math.round(counter + quarterOfTotalReferences);
+      counter += quarterOfTotalReferences;
+    }
+
+    for (let i = 0; i < bottomBarValues.length; i += 1) {
+      bottomBarValues[i].innerText = topBarValues[i].innerText;
+    }
+
+    for (let i = 0; i < referencesValuesElements.length; i += 1) {
+      referencesValuesElements[i].innerText = `${referencesByDays[i] === null ? `0` : referencesByDays[i]}`;
+      referencesValuesElements[i].style.width = `${referencesByDays[i] === 0 ? '16px' : `${(referencesByDays[i] / totalReferences) * 100}%`}`;
+    }
+    
     console.log(referencesByDays);
+    console.log(totalReferences);
   }
 }
