6d3c4d90e53295d3c84696d68d1dae75bfc2d604
Alexey Lebedev <vorresti@ya.ru>
2020-08-03 12:31:04 +0300

    get-mentions-in-titles: cycle "for" replaced with map

--- src/js/utils/get-mentions-in-titles.js
+++ src/js/utils/get-mentions-in-titles.js
@@ -1,9 +1,9 @@
 export default function getMentionsInTitles(request, articles) {
-  const articlesTitles = [];
   const regExp = new RegExp(request.toLowerCase(), 'g');
-  for (let artickle of articles) {
-    articlesTitles.push(artickle.title);
-  }
+
+  const articlesTitles = [];
+  articles.map( article => articlesTitles.push(article.title) );
+
   const result = String(articlesTitles).toLowerCase().match(regExp);
   return result ? result.length : 0;
 }
